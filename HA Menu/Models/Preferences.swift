d41da3d4f92a11885e9fe2a592af9488b1c80084
Andrew Jackson <andrew@codechimp.org>
2019-11-11 16:01:40 +0000

    Remove obsolete tokens

--- HA Menu/Models/Preferences.swift
+++ HA Menu/Models/Preferences.swift
@@ -10,14 +10,6 @@ import Foundation
 
 struct Preferences {
 
-    // Witham
-    //    var server = "http://192.168.3.142:8123"
-    //    var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkNjBjOWUxNTVhMjA0OGY3YmFlNmM0MWM1MWZiYjdkYiIsImlhdCI6MTU0MDA1NTA1NSwiZXhwIjoxODU1NDE1MDU1fQ.gmE5MXpcLL7qKsDLddWlb5IflSi6GJm4Cej6BMBHpKA"
-
-    // Ipswich
-    //    var server = "http://192.168.1.25:8123"
-    //    var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJlZmFiNDIzNjJhZTk0NmM3YThiNzIwZWFhYmI5ZGQzZiIsImlhdCI6MTU0MDY1ODk4NiwiZXhwIjoxODU2MDE4OTg2fQ.7A5tcAEehwDeg8hDViYbWKcf9wZGQ15areD9-naG5tw"
-
     var server: String {
         get {
             if(UserDefaults.standard.value(forKey: "server") == nil) {
